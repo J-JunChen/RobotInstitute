b86872ca3d238033dc4acfe30c9532a332b5f571
JunStitch <446218580@qq.com>
2018-04-18 10:30:32 +0800

    添加‘‘助班’用户类型，方便审核添加小班信息

---
+++ Robot/Admin/View/Helper/index.php
@@ -0,0 +1,96 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="__PUBLIC__/robot.ico"/>
+
+    <!-- Main CSS-->
+    <link rel="stylesheet" type="text/css" href="__PUBLIC__/css/Admin/main.css">
+
+    <link href="https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <title>机器人学院后台管理系统</title>
+
+   <!-- 通过cdn添加css和js  -->
+     <link href="__PUBLIC__/css/Admin/bootstrap.min.css" rel="stylesheet">
+
+ 
+    </head>
+    <body class="app sidebar-mini">
+
+   <!-- 引入目录文件 -->
+
+    <include file="helper_menu" />;
+
+    <main class="app-content">
+      <div class="app-title">
+        <div>
+          <h1><i class="fa fa-pie-chart"></i> 图表</h1>
+          <h5>关于机器人学院网站的数据分析</h5>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="tile">
+            <h3 class="tile-title">机器人学院一周内每日PV<span style="color: red;">（待申请到学校的二级域名再统计）</span></h3>
+            <div class="embed-responsive embed-responsive-16by9">
+              <canvas class="embed-responsive-item" id="lineChartDemo"></canvas>
+            </div>
+          </div>
+        </div>
+       
+        <div class="col-md-6">
+          <div class="tile">
+            <h3 class="tile-title">注册用户各专业比例</h3>
+            <div class="embed-responsive embed-responsive-16by9">
+              <canvas class="embed-responsive-item" id="pieChartDemo"></canvas>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="tile">
+            <h3 class="tile-title">注册用户师生比例</h3>
+            <div class="embed-responsive embed-responsive-16by9">
+              <canvas class="embed-responsive-item" id="doughnutChartDemo"></canvas>
+            </div>
+          </div>
+        </div>
+
+         <div class="col-md-6">
+          <div class="tile">
+            <h3 class="tile-title">注册用户男女比例</h3>
+            <div class="embed-responsive embed-responsive-16by9">
+              <canvas class="embed-responsive-item" id="polarChartDemo"></canvas>
+            </div>
+          </div>
+        </div>
+
+        
+      </div>
+    </main>
+
+    <!-- 添加jq.js，注意，所有的js必须在jquery.js的后面，因为所有的js都是基于jq -->
+    <script src="__PUBLIC__/js/Admin/jquery.min.js"></script> 
+    <!-- 添加bootstrap框架的js -->
+    <script src="__PUBLIC__/js/Admin/bootstrap.min.js"></script>
+
+    <script src="__PUBLIC__/js/Admin/popper.min.js"></script>
+    <!-- <script src="https://cdn.bootcss.com/popper.js/1.13.0/popper.min.js"></script> -->
+
+
+    <script src="__PUBLIC__/js/Admin/main.js"></script>
+
+    <!-- The javascript plugin to display page loading on top-->
+    <script src="__PUBLIC__/js/Admin/pace.min.js"></script>
+    <!-- <script src="https://cdn.bootcss.com/pace/1.0.2/pace.min.js"></script> -->
+
+    <!-- Page specific javascripts-->
+    <script type="text/javascript" src="__PUBLIC__/js/Admin/chart.js"></script>
+
+    <!-- 主页数据分析 -->
+    <script type="text/javascript" src="__PUBLIC__/js/Admin/analyze.js"></script>
+
+  </body>
+</html>
